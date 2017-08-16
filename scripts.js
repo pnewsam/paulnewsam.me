78bd46a7c0a2a298d8fc1a588561961410baae1f
monkypuzzle <paul.newsam@gmail.com>
2017-08-16 16:16:18 -0700

    Cleans up js, adjusts card styling

--- scripts.js
+++ scripts.js
@@ -26,15 +26,3 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
-
-// $(document).ready(function(){
-//   // $(alert('ready!'));
-//   $(".past-projects .card").on("mouseover",function(){
-//     console.log('fired!')
-//     $(this).find(".past-projects__filter").removeClass("hide").fadeIn({duration:400, easing: easeInQuint})
-//   });
-//   $(".past-projects .card").on("mouseout",function(){
-//     console.log('fired!')
-//     $(this).find(".past-projects__filter").addClass("hide").fadeOut({duration:400, easing: easeInQuint})
-//   });
-// });
